97374650bf970e48d17bf7745143c3f02089d145
ndconger <ndconger@email.wm.edu>
2025-05-11 14:01:23 -0400

    full set up for model training

--- ball_event_train.py
+++ ball_event_train.py
@@ -7,20 +7,19 @@ from tensorflow.keras.optimizers.schedules import ExponentialDecay
 import numpy as np
 
 def get_frames_set(ball_json, event_json):
+    #open ball positions
     with open(ball_json, 'r') as f:
         ball_data = json.load(f)
+    #open event positions and transform classes to model output value
     with open(event_json, 'r') as f:
         events = {"bounce": np.array([1,0,0]), "net": np.array([0,1,0]), "empty_event": np.array([0,0,1])}
         event_data = json.load(f)
-        for frame, event in event_data.items():
-            print(frame, event)
         event_data = {frame: events[event] for frame, event in event_data.items()}
-    print(event_data)
+    #find intersection b/w keys of ball and event data 
+    #commonality acts as the training set
     train_frames = set(event_data.keys()) & set(ball_data.keys())
     ball_data = {frame: ball_data[frame] for frame in train_frames}
     event_data = {frame: event_data[frame] for frame in train_frames}
-    print("EVENT DATA KEYS:", event_data.keys())
-    print("BALL DATA KEYS:", ball_data.keys())
     print(f"Train frames: {len(train_frames)}")
     print(f"Ball data: {len(ball_data)}")
     print(f"Event data: {len(event_data)}")
@@ -85,18 +84,14 @@ def extract_specific_frames(video_path, frame_indices):
 def data_preprocess(video_path, ball_json, event_json):
     # Step 1: Get frames set
     train_frames, ball_data, event_data = get_frames_set(ball_json, event_json)
-    #print(f"Train frames: {len(train_frames)}")
+    
     train_frames = list(map(int, event_data.keys()))
     print("HERE ARE THE TRAIN FRAMES")
     #print(train_frames)
     frames = extract_specific_frames(video_path, train_frames)
-    #print(frames)
-    print(f"Frames shape: {len(frames)}, {frames[44].shape}")
     # Step 3: Center crop frames
     cropped_frames = []
-    print(ball_data)
     for frame in frames:
-        print(frame)
         frame_num = str(frame)
         center = (int(ball_data[frame_num]['x']), int(ball_data[frame_num]['y']))
         cropped_frame = crop_centered_with_padding(frames[frame], center, (320, 220))
@@ -127,12 +122,14 @@ def create_event_predictor_model(input_shape=(224, 224, 3)):
 # Step 4: Training
 def train_event_predictor(frames, event_data, model_save_path="ball_event_model.h5", batch_size=16, epochs=10):
     #dataset = BallTrackingDataset(video_path, label_json_path, batch_size=batch_size)
-    print("TRAIN EVENT PREDICTOR\n\n\n\n")
-    print(len(frames), len(event_data))
-    print(f"Frames shape: {frames[0].shape}")
+    # print("TRAIN EVENT PREDICTOR")
+    # print(len(frames), len(event_data))
+    # print(f"Frames shape: {frames[0].shape}")
+
+    #convert to usable format
     frames = np.array(frames, dtype=np.float32) / 255.0
     event_data = np.array(list(event_data.values()), dtype=np.float32)
-    print(f"Event data shape: {event_data[0].shape}")
+    
     if os.path.exists(model_save_path):
         print("LOADING EXISTING MODEL")
         model = tf.keras.models.load_model(model_save_path)
@@ -142,7 +139,7 @@ def train_event_predictor(frames, event_data, model_save_path="ball_event_model.
         model = create_event_predictor_model(input_shape=(320, 220, 3))
     model.fit(frames, event_data, batch_size=batch_size, epochs=epochs)
     model.save(model_save_path)
-    print(f"✅ Model saved to {model_save_path}")
+    print(f"Model saved to {model_save_path}")
 
 # Full Pipeline
 def full_pipeline(video_path, ball_json_path, event_json_path):
